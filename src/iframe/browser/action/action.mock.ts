6f45b14aa9c572c17395ee04522976b9f8984313
Dani Gámez Franco <gmzcodes@outlook.com>
2025-04-02 15:11:28 +0200

    Delete old backend API SDK scaffolding.

--- src/iframe/browser/action/action.mock.ts
+++ src/iframe/browser/action/action.mock.ts
@@ -1,3 +1,5 @@
 export const action = {
+  // This mock exists jus in case we call this by mistake in Embed but, in general, any code that calls `action.setIcon`
+  // should only do it for BE and have some other behavior implemented for Embed.
   setIcon: (options: any) => {}
 };
