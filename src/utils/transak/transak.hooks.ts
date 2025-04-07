e7a2e6726857f8e8c9e30f4279ce4a7d479cf886
Pawan Paudel <pawanpaudel93@gmail.com>
2025-04-07 18:30:39 +0545

    refactor: optimize useTransak hook

--- src/utils/transak/transak.hooks.ts
+++ src/utils/transak/transak.hooks.ts
@@ -35,14 +35,14 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
   });
 
   // Handle amount input change
-  const handleAmountChange = (value: string) => {
+  const handleAmountChange = useCallback((value: string) => {
     if (/^(\d*\.?\d*)$/.test(value) || value === "") {
       setPurchaseAmount(value);
     }
-  };
+  }, []);
 
   // Process quote result
-  const finishUp = (quote: Quote | null) => {
+  const finishUp = useCallback((quote: Quote | null) => {
     if (quote) {
       const rate = (quote.fiatAmount - quote.totalFee) / quote.cryptoAmount;
       setExchangeRate(rate);
@@ -53,7 +53,7 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
     }
     setQuote(quote);
     setLoading(false);
-  };
+  }, []);
 
   // Handle currency selection
   const handleUpdateCurrency = useCallback((currency: any) => {
@@ -68,7 +68,7 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
   }, []);
 
   // Function to get display amount for USD/fiat equivalent
-  const getDisplayAmount = () => {
+  const getDisplayAmount = useCallback(() => {
     if (arConversion) {
       const symbol = getSymbolFromCurrency(selectedCurrency?.symbol || "USD");
       // If arConversion is true, show the fiat equivalent
@@ -79,7 +79,94 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
       // If arConversion is false, show the AR equivalent
       return `${quote?.cryptoAmount.toFixed(6) || "0.00"} AR`;
     }
-  };
+  }, [arConversion, selectedCurrency, quote]);
+
+  const fetchQuote = useCallback(async () => {
+    setLoading(true);
+    setQuote(null);
+    if (
+      Number(debouncedAmount) <= 0 ||
+      debouncedAmount === "" ||
+      !selectedCurrency ||
+      !paymentMethod
+    ) {
+      finishUp(null);
+      return;
+    }
+    if (
+      !arConversion &&
+      (+debouncedAmount > paymentMethod.maxAmount ||
+        +debouncedAmount < paymentMethod.minAmount)
+    ) {
+      const isExceedMaxAmount = +debouncedAmount > paymentMethod.maxAmount;
+      setError(
+        browser.i18n.getMessage(
+          isExceedMaxAmount ? "max_buy_amount" : "min_buy_amount",
+          [
+            isExceedMaxAmount
+              ? paymentMethod.maxAmount
+              : paymentMethod.minAmount,
+            selectedCurrency?.symbol
+          ]
+        )
+      );
+      finishUp(null);
+      return;
+    }
+    const baseUrl = `${BASE_URL}/api/v1/pricing/public/quotes`;
+    const params = new URLSearchParams({
+      partnerApiKey: apiKey,
+      fiatCurrency: selectedCurrency?.symbol,
+      cryptoCurrency: "AR",
+      isBuyOrSell: "BUY",
+      network: "mainnet",
+      paymentMethod: paymentMethod.id
+    });
+    if (arConversion) {
+      params.append("cryptoAmount", debouncedAmount);
+    } else {
+      params.append("fiatAmount", debouncedAmount);
+    }
+
+    if (countryCode) {
+      params.append("quoteCountryCode", countryCode);
+    }
+
+    const url = `${baseUrl}?${params.toString()}`;
+
+    try {
+      const response = await retryWithDelay(() => fetch(url));
+      if (!response.ok) {
+        try {
+          const resJson = await response.json();
+          if (resJson?.error?.message) {
+            setError(resJson?.error?.message);
+          } else {
+            throw new Error("Network response was not ok");
+          }
+        } catch {
+          setError(browser.i18n.getMessage("transak_unavailable"));
+        }
+        finishUp(null);
+        return;
+      }
+      setError(null);
+      const data = await response.json();
+      const updatedQuote = data.response as Quote;
+      finishUp(updatedQuote);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(browser.i18n.getMessage("transak_unavailable"));
+      finishUp(null);
+    }
+  }, [
+    apiKey,
+    debouncedAmount,
+    selectedCurrency,
+    paymentMethod,
+    arConversion,
+    countryCode
+  ]);
 
   // Fetch available currencies
   useEffect(() => {
@@ -126,86 +213,6 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
 
   // Fetch quote when amount changes
   useEffect(() => {
-    const fetchQuote = async () => {
-      setLoading(true);
-      setQuote(null);
-      if (
-        Number(debouncedAmount) <= 0 ||
-        debouncedAmount === "" ||
-        !selectedCurrency ||
-        !paymentMethod
-      ) {
-        finishUp(null);
-        return;
-      }
-      if (
-        !arConversion &&
-        (+debouncedAmount > paymentMethod.maxAmount ||
-          +debouncedAmount < paymentMethod.minAmount)
-      ) {
-        const isExceedMaxAmount = +debouncedAmount > paymentMethod.maxAmount;
-        setError(
-          browser.i18n.getMessage(
-            isExceedMaxAmount ? "max_buy_amount" : "min_buy_amount",
-            [
-              isExceedMaxAmount
-                ? paymentMethod.maxAmount
-                : paymentMethod.minAmount,
-              selectedCurrency?.symbol
-            ]
-          )
-        );
-        finishUp(null);
-        return;
-      }
-      const baseUrl = `${BASE_URL}/api/v1/pricing/public/quotes`;
-      const params = new URLSearchParams({
-        partnerApiKey: apiKey,
-        fiatCurrency: selectedCurrency?.symbol,
-        cryptoCurrency: "AR",
-        isBuyOrSell: "BUY",
-        network: "mainnet",
-        paymentMethod: paymentMethod.id
-      });
-      if (arConversion) {
-        params.append("cryptoAmount", debouncedAmount);
-      } else {
-        params.append("fiatAmount", debouncedAmount);
-      }
-
-      if (countryCode) {
-        params.append("quoteCountryCode", countryCode);
-      }
-
-      const url = `${baseUrl}?${params.toString()}`;
-
-      try {
-        const response = await retryWithDelay(() => fetch(url));
-        if (!response.ok) {
-          try {
-            const resJson = await response.json();
-            if (resJson?.error?.message) {
-              setError(resJson?.error?.message);
-            } else {
-              throw new Error("Network response was not ok");
-            }
-          } catch {
-            setError(browser.i18n.getMessage("transak_unavailable"));
-          }
-          finishUp(null);
-          return;
-        }
-        setError(null);
-        const data = await response.json();
-        const updatedQuote = data.response as Quote;
-        finishUp(updatedQuote);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError(browser.i18n.getMessage("transak_unavailable"));
-        finishUp(null);
-      }
-    };
-
     if (debouncedAmount) {
       fetchQuote();
     } else {
@@ -213,7 +220,7 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
       setExchangeRate(0);
       setUnavailableQuote(false);
     }
-  }, [debouncedAmount, selectedCurrency, paymentMethod, arConversion, apiKey]);
+  }, [debouncedAmount, fetchQuote]);
 
   // Update validation when AR conversion changes
   useEffect(() => {
@@ -248,57 +255,63 @@ export const useTransak = (apiKey: string, initialConversion = false) => {
   }, [selectedCurrency]);
 
   // Create a Transak purchase URL
-  const createPurchaseUrl = (walletAddress: string) => {
-    if (!quote) return null;
+  const createPurchaseUrl = useCallback(
+    (walletAddress: string) => {
+      if (!quote) return null;
 
-    const baseUrl = "https://global.transak.com/";
-    const params = new URLSearchParams({
-      apiKey: apiKey,
-      defaultCryptoCurrency: "AR",
-      defaultFiatAmount: quote.fiatAmount.toString(),
-      defaultFiatCurrency: quote.fiatCurrency,
-      walletAddress: walletAddress,
-      defaultPaymentMethod: quote.paymentMethod
-    });
+      const baseUrl = "https://global.transak.com/";
+      const params = new URLSearchParams({
+        apiKey: apiKey,
+        defaultCryptoCurrency: "AR",
+        defaultFiatAmount: quote.fiatAmount.toString(),
+        defaultFiatCurrency: quote.fiatCurrency,
+        walletAddress: walletAddress,
+        defaultPaymentMethod: quote.paymentMethod
+      });
 
-    return `${baseUrl}?${params.toString()}`;
-  };
+      return `${baseUrl}?${params.toString()}`;
+    },
+    [apiKey, quote]
+  );
 
-  const openCurrencySelector = (e?: React.MouseEvent) => {
+  const openCurrencySelector = useCallback((e?: React.MouseEvent) => {
     if (e) e.preventDefault();
     setShowCurrencySelector(true);
-  };
+  }, []);
 
-  const openPaymentSelector = (e?: React.MouseEvent) => {
+  const openPaymentSelector = useCallback((e?: React.MouseEvent) => {
     if (e) e.preventDefault();
     setShowPaymentSelector(true);
-  };
+  }, []);
 
-  const closeCurrencySelector = (e?: React.MouseEvent) => {
+  const closeCurrencySelector = useCallback((e?: React.MouseEvent) => {
     if (e) e.preventDefault();
     setShowCurrencySelector(false);
-  };
+  }, []);
 
-  const closePaymentSelector = (e?: React.MouseEvent) => {
+  const closePaymentSelector = useCallback((e?: React.MouseEvent) => {
     if (e) e.preventDefault();
     setShowPaymentSelector(false);
-  };
+  }, []);
 
-  const openTransak = async (navigateTo: string) => {
-    try {
-      const url = createPurchaseUrl(activeAddress);
-      if (url) {
-        if (IS_EMBEDDED_APP) {
-          window.open(url, "_blank");
-        } else {
-          browser.tabs.create({ url });
+  const openTransak = useCallback(
+    async (navigateTo: string) => {
+      try {
+        const url = createPurchaseUrl(activeAddress);
+        if (url) {
+          if (IS_EMBEDDED_APP) {
+            window.open(url, "_blank");
+          } else {
+            browser.tabs.create({ url });
+          }
+          navigate(navigateTo as WanderRoutePath);
         }
-        navigate(navigateTo as WanderRoutePath);
+      } catch (error) {
+        console.error("Error buying AR:", error);
       }
-    } catch (error) {
-      console.error("Error buying AR:", error);
-    }
-  };
+    },
+    [activeAddress, createPurchaseUrl, navigate]
+  );
 
   return {
     // State
