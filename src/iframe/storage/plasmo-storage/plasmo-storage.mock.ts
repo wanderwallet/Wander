2448967fef545591dabb000de85c7a3b5957466e
Dani Gámez Franco <gmzcodes@outlook.com>
2025-04-24 20:09:40 +0200

    Revert "feat: add partitioned storage banner"

--- src/iframe/storage/plasmo-storage/plasmo-storage.mock.ts
+++ src/iframe/storage/plasmo-storage/plasmo-storage.mock.ts
@@ -41,6 +41,7 @@ export interface StorageMockInterface extends PlasmoStorage {
     expiresIn?: number
   ): Promise<void>;
   requestStorageAccess(): Promise<void>;
+  requestAccessOnUserInteraction(): Promise<void>;
   hasAvailableSpace(bytesNeeded: number): Promise<boolean>;
   getRaw(key: string): Promise<string | null>;
   setRaw(key: string, value: string): Promise<void>;
@@ -49,12 +50,10 @@ export interface StorageMockInterface extends PlasmoStorage {
 
 export class StorageMock extends PlasmoStorage implements StorageMockInterface {
   private storage: EnhancedStorage;
-  private _area: "session" | "local" = "session";
 
   constructor(area: "session" | "local" = "session") {
     super({ area });
     this.storage = new EnhancedStorage({ area });
-    this._area = area;
 
     // This browser doesn't support the Storage Access API
     // so let's just hope we have access!
@@ -74,7 +73,7 @@ export class StorageMock extends PlasmoStorage implements StorageMockInterface {
   }
 
   get area(): "session" | "sync" | "local" | "managed" {
-    return this._area;
+    return "session";
   }
 
   get hasWebApi(): boolean {
@@ -165,6 +164,10 @@ export class StorageMock extends PlasmoStorage implements StorageMockInterface {
   // unpartitioned storage:
   async requestStorageAccess() {
     await this.storage.requestStorageAccess();
+  }
+
+  async requestAccessOnUserInteraction() {
+    await this.storage.requestAccessOnUserInteraction();
   }
 
   // Additional methods:
