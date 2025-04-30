e3df86e41862f5c9fda99dcfb940f682e478e27a
Dani Gámez Franco <gmzcodes@outlook.com>
2025-04-30 17:07:55 +0200

    Add sign out button under the Actions tab.

--- src/routes/embedded/wallet/home/actions.container.tsx
+++ src/routes/embedded/wallet/home/actions.container.tsx
@@ -4,8 +4,11 @@ import {
   Text,
   ReceiptIcon,
   OpenTabIcon,
+  XClose,
   Box
 } from "~components/embed/ui";
+import browser from "~iframe/browser";
+import { signOut } from "~utils/embedded/embedded.utils";
 import { Link } from "~wallets/router/components/link/Link";
 
 export function WalletHomeActions() {
@@ -29,7 +32,9 @@ export function WalletHomeActions() {
           </Text>
         </Row>
       </Link>
+
       <br />
+
       <Link
         to="/wallet/transactions"
         style={{ textDecoration: "none", width: "100%" }}
@@ -41,13 +46,35 @@ export function WalletHomeActions() {
             cursor: "pointer"
           }}
         >
-          <ReceiptIcon />
+          <ReceiptIcon color="#121212" />
           <Text variant="bodyMd" style={{ color: "#121212" }}>
             Transaction history
           </Text>
         </Row>
       </Link>
+
       <br />
+
+      <button
+        onClick={signOut}
+        style={{ textDecoration: "none", width: "100%" }}
+      >
+        <Row
+          alignment="center"
+          justifyContent="start"
+          style={{
+            cursor: "pointer"
+          }}
+        >
+          <XClose color="#121212" />
+          <Text variant="bodyMd" style={{ color: "#121212" }}>
+            {browser.i18n.getMessage("sign_out")}
+          </Text>
+        </Row>
+      </button>
+
+      <br />
+
       {/* <Link
         to="/wallet/settings"
         style={{ textDecoration: "none", width: "100%" }}
