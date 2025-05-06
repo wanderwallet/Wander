55d68a15e8249f80c0bce133a0b9a3002ff4db5a
Dani Gámez Franco <gmzcodes@outlook.com>
2025-05-06 15:23:24 +0200

    Fix isAnchor property being added as an HTML attribute, and fix redirect after wallet backup not working.

--- src/components/embed/ui/atoms/loading/Loading.tsx
+++ src/components/embed/ui/atoms/loading/Loading.tsx
@@ -3,7 +3,13 @@ import clsx from "clsx";
 import styles from "./Loading.module.css";
 import type { LoadingBaseProps } from "./Loading.types";
 
-const Loading = forwardRef<HTMLDivElement, LoadingBaseProps>(({ size = "md", color, className, ...props }, ref) => {
+const Loading = forwardRef<HTMLDivElement, LoadingBaseProps>(({
+  className,
+  size = "md",
+  color,
+  isAnchor,
+  ...props
+}, ref) => {
   return (
     <div
       className={clsx(styles.loading, styles[`loading__${size}`], className)}
