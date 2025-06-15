2133e9ad335fe895269a94b7ada9d7e1b9137c04
Marton Lederer <marton@lederer.hu>
2025-06-15 08:19:59 +0200

    fix: free liquidops client wallet on error

--- src/routes/popup/agents/liquidops/utils/LiquidOps.ts
+++ src/routes/popup/agents/liquidops/utils/LiquidOps.ts
@@ -6,46 +6,54 @@ import { freeDecryptedWallet } from "~wallets/encryption";
 import type { JWKInterface } from "arweave/web/lib/wallet";
 
 export const LiquidOpsClient = async (useWallet = false) => {
-  const decryptedWallet: DecryptedWallet = useWallet
-    ? await getActiveKeyfile()
-    : {
-        type: "local",
-        nickname: "",
-        address: "",
-        keyfile: {} as JWKInterface,
+  let decryptedWallet: DecryptedWallet;
+
+  try {
+    decryptedWallet = useWallet
+      ? await getActiveKeyfile()
+      : {
+          type: "local",
+          nickname: "",
+          address: "",
+          keyfile: {} as JWKInterface,
+        };
+    isLocalWallet(decryptedWallet);
+
+    const createDataItemSigner =
+      (wallet: any) =>
+      async ({
+        data,
+        tags = [],
+        target,
+        anchor,
+      }: {
+        data: any;
+        tags?: { name: string; value: string }[];
+        target?: string;
+        anchor?: string;
+      }): Promise<{ id: string; raw: ArrayBuffer }> => {
+        const signer = new ArweaveSigner(wallet);
+        const dataItem = createData(data, signer, { tags, target, anchor });
+
+        await dataItem.sign(signer);
+
+        return {
+          id: dataItem.id,
+          // @ts-expect-error
+          raw: dataItem.getRaw(),
+        };
       };
-  isLocalWallet(decryptedWallet);
 
-  const createDataItemSigner =
-    (wallet: any) =>
-    async ({
-      data,
-      tags = [],
-      target,
-      anchor,
-    }: {
-      data: any;
-      tags?: { name: string; value: string }[];
-      target?: string;
-      anchor?: string;
-    }): Promise<{ id: string; raw: ArrayBuffer }> => {
-      const signer = new ArweaveSigner(wallet);
-      const dataItem = createData(data, signer, { tags, target, anchor });
+    const signer = createDataItemSigner(decryptedWallet.keyfile);
+    const client = new LiquidOps(signer);
 
-      await dataItem.sign(signer);
-
-      return {
-        id: dataItem.id,
-        // @ts-expect-error
-        raw: dataItem.getRaw(),
-      };
+    return {
+      client,
+      free: () => freeDecryptedWallet(decryptedWallet.keyfile),
     };
-
-  const signer = createDataItemSigner(decryptedWallet.keyfile);
-  const client = new LiquidOps(signer);
-
-  return {
-    client,
-    free: () => freeDecryptedWallet(decryptedWallet.keyfile),
-  };
+  } catch {
+    if (decryptedWallet && decryptedWallet.type === "local") {
+      freeDecryptedWallet(decryptedWallet.keyfile);
+    }
+  }
 };
