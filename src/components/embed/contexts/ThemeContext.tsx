d7d68850408d8bca56970f5a88f53aae0b9ed4ad
Dani Gámez Franco <gmzcodes@outlook.com>
2025-06-16 12:16:06 +0200

    Update ThemeProvider to show the cover when changing themes.

--- src/components/embed/contexts/ThemeContext.tsx
+++ src/components/embed/contexts/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect, useContext, type ReactNode } from "react";
+import { createContext, useState, useEffect, useContext, type ReactNode, useCallback, useRef } from "react";
 import { themeTokens, commonTokens } from "../themes/theme-config";
+import { EMBEDDED_THEME } from "~utils/embedded/iframe.utils";
 
 export type ThemeMode = "light" | "dark" | "system";
 
@@ -22,7 +23,16 @@ const defaultContext: ThemeContextType = {
 const ThemeContext = createContext<ThemeContextType>(defaultContext);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [mode, setMode] = useState<ThemeMode>("system");
+  const [mode, setMode] = useState<ThemeMode>(() => {
+    const themeParam = new URLSearchParams(window.location.search).get("theme") as ThemeMode | null;
+    if (themeParam && ["light", "dark", "system"].includes(themeParam)) return themeParam;
+
+    const savedTheme = localStorage.getItem("theme") as ThemeMode | null;
+    if (savedTheme && ["light", "dark", "system"].includes(savedTheme)) return savedTheme;
+
+    return "system";
+  });
+
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
@@ -48,24 +58,6 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   }, [mode]);
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const themeParam = urlParams.get("theme") as ThemeMode | null;
-
-    if (themeParam && ["light", "dark", "system"].includes(themeParam)) {
-      setMode(themeParam);
-      return;
-    }
-
-    const savedTheme = localStorage.getItem("theme") as ThemeMode | null;
-    if (savedTheme && ["light", "dark", "system"].includes(savedTheme)) {
-      setMode(savedTheme);
-      return;
-    }
-
-    setMode("system");
-  }, []);
-
-  useEffect(() => {
     localStorage.setItem("theme", mode);
 
     if (window.parent !== window) {
@@ -73,11 +65,43 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
     }
   }, [mode, isDarkMode]);
 
+  const lastModeRef = useRef(mode);
+  const setModeTimeoutIDRef = useRef(0);
+
+  lastModeRef.current = mode;
+
+  const handleSetMode = useCallback((mode: ThemeMode) => {
+    const cover = document.getElementById("cover");
+
+    if (mode === lastModeRef.current || !cover) return;
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const appliedMode = mediaQuery.matches ? "dark" : "light";
+
+    if (
+      (mode === "system" && appliedMode === lastModeRef.current) ||
+      (lastModeRef.current === "system" && appliedMode === mode)
+    ) {
+      // If mode is (`mode` arg) or was (`lastModeRef.current`) "system" and the
+      // applied theme doesn't change, no need to show the cover, just update the state.
+      setMode(mode);
+
+      return;
+    }
+
+    window.clearTimeout(setModeTimeoutIDRef.current);
+    cover.removeAttribute("aria-hidden");
+    setMode(mode);
+    setModeTimeoutIDRef.current = window.setTimeout(() => {
+      cover.setAttribute("aria-hidden", "true");
+    }, 230);
+  }, []);
+
   return (
     <ThemeContext.Provider
       value={{
         mode,
-        setMode,
+        setMode: handleSetMode,
         isDarkMode,
         tokens: themeTokens,
         common: commonTokens,
