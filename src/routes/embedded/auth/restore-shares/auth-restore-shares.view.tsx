2e53032017c4135df566edc6771e379883f0cf91
Dani Gámez Franco <gmzcodes@outlook.com>
2025-06-27 20:27:11 +0200

    WIP changes on AuthRestoreShares view to handle unrecoverable wallets.

--- src/routes/embedded/auth/restore-shares/auth-restore-shares.view.tsx
+++ src/routes/embedded/auth/restore-shares/auth-restore-shares.view.tsx
@@ -1,17 +1,81 @@
 import { QrCode02 } from "@untitled-ui/icons-react";
-import { Button, WalletIcon, SeedIcon, KeyIcon } from "~components/embed/ui";
+import { useMemo } from "react";
+import {
+  Button,
+  WalletIcon,
+  SeedIcon,
+  KeyIcon,
+  Text,
+  Snackbar,
+  type SnackbarVariant,
+  Divider,
+} from "~components/embed/ui";
 import { OnboardingCard } from "~components/embed/ui/molecules/card/onboarding-card/OnboardingCard";
 import { useEmbedded } from "~utils/embedded/embedded.hooks";
 import { signOut } from "~utils/embedded/embedded.utils";
 
 export function AuthRestoreSharesEmbeddedView() {
-  // TODO: Automatically retry loading wallets once before showing this screen?
+  const { wallets, unpartitionedStateStatus } = useEmbedded();
+  const hasMultipleWallets = wallets.length > 1;
+  const hasRecoverableWallets = wallets.some((wallet) => wallet.canBeRecovered) || true;
+
+  let createFirst = false;
+  let snackbarVariant: SnackbarVariant = "warning";
+  let title = "Using a new device?";
+  let message =
+    "This happens when you start using Wander on a new device or browser, or clear your browser data. If you've lost your backup options, you can create a new one instead.";
+  let unpartitionedStorageMessage = "";
+  let showSupportLinks = false;
+  let needsWalletCreationConfirmation = false;
+
+  // TODO: Let them know what type of backup/export they made and where? Let them check all available options?
+  const lastRecovery = useMemo(() => {
+    let lastRecovery = 0;
+
+    wallets.forEach(({ lastBackedUpAt, lastExportedAt }) => {
+      lastRecovery = Math.max(lastRecovery, lastBackedUpAt?.getTime() || 0, lastExportedAt?.getTime() || 0);
+    });
+  }, []);
+
+  // Keep in mind users could have seen the QR or seedphrase backup but then they might not scan or save it, respectively. Also, users might have exported
+  // a keyfile or recovery file, but they might have lost it.
+
+  if (!hasRecoverableWallets) {
+    createFirst = true;
+    showSupportLinks = true;
+    snackbarVariant = "error";
+    title = "No backups detected";
+    message = hasMultipleWallets
+      ? `It looks like your wallets are not recoverable. Unless you backed any of them up, they are lost forever. Create a new wallet instead.`
+      : `It looks like your wallet is not recoverable. Unless you backed it up, it's lost forever. Create a new wallet instead.`;
+  } else if (unpartitionedStateStatus !== "supported") {
+    // TODO: Let them know where they've previously signed in?
+    showSupportLinks = true;
+    snackbarVariant = "error";
+    unpartitionedStorageMessage =
+      "If you've previously used Wander on a different site, your might still be able to backup your wallet from it. Please, go back and try to backup your wallet.";
+  }
 
   return (
     <OnboardingCard
       headerText="Restore wallet"
       subtitle="Select a method for restoring your wallet."
       onBackButtonClick={() => signOut(false)}>
+      <Snackbar title={title} variant={snackbarVariant}>
+        <p>{message}</p>
+        {unpartitionedStorageMessage ? <p>{unpartitionedStorageMessage} </p> : null}
+      </Snackbar>
+
+      {createFirst ? (
+        <>
+          <Button variant="outlined" isFullWidth icon={<WalletIcon fontSize={24} />} href="/auth/add-wallet">
+            Create new wallet
+          </Button>
+
+          <Divider text={"OR"} />
+        </>
+      ) : null}
+
       {/* <Button
         variant="outlined"
         isFullWidth
@@ -61,6 +125,16 @@ export function AuthRestoreSharesEmbeddedView() {
         href="/auth/restore-shares/qrcode">
         Scan QR Code
       </Button>
+
+      {createFirst ? null : (
+        <>
+          <Divider text={"OR"} />
+
+          <Button variant="outlined" isFullWidth icon={<WalletIcon fontSize={24} />} href="/auth/add-wallet">
+            Create new wallet
+          </Button>
+        </>
+      )}
     </OnboardingCard>
   );
 }
