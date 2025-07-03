214026c41ed8cef450896bd5131b1efa6eeb318c
Dani Gámez Franco <gmzcodes@outlook.com>
2025-07-03 19:26:35 +0200

    Add change password action button.

--- src/routes/embedded/wallet/home/actions/actions.container.tsx
+++ src/routes/embedded/wallet/home/actions/actions.container.tsx
@@ -2,24 +2,25 @@ import { CoinsIcon, ReceiptIcon, XClose, Box } from "~components/embed/ui";
 import browser from "~iframe/browser";
 import { ActionItem } from "~routes/embedded/wallet/home/actions/action-item";
 import { signOut } from "~utils/embedded/embedded.utils";
+import { Lock01 } from "@untitled-ui/icons-react";
+import { useEmbedded } from "~utils/embedded/embedded.hooks";
 
 export function WalletHomeActions() {
+  const { user } = useEmbedded();
+
   return (
     <Box style={{ padding: 0 }}>
-      <ActionItem
-        label="Receive tokens"
-        icon={ CoinsIcon }
-        to="/wallet/receive/options" />
+      <ActionItem label="Receive tokens" icon={CoinsIcon} to="/wallet/receive/options" />
+
+      <ActionItem label="Transaction history" icon={ReceiptIcon} to="/wallet/transactions" />
 
       <ActionItem
-        label="Transaction history"
-        icon={ ReceiptIcon }
-        to="/wallet/transactions" />
+        label={browser.i18n.getMessage(user.user_metadata.hasPassword ? "change_password" : "set_password")}
+        icon={Lock01}
+        to="/account/change-password"
+      />
 
-      <ActionItem
-        label={browser.i18n.getMessage("sign_out")}
-        icon={ XClose }
-        onClick={() => signOut()} />
+      <ActionItem label={browser.i18n.getMessage("sign_out")} icon={XClose} onClick={() => signOut()} />
 
       {/* <Link
         to="/wallet/settings"
