7adf4867d079fba7bbb03cc0c620d7ea4327ba88
Pawan Paudel <pawanpaudel93@gmail.com>
2025-07-03 14:24:17 +0545

    feat: enhance TierTag and WanderIcon components with dynamic box shadows and updated styling for improved visual representation

--- src/components/popup/tier/TierTag.tsx
+++ src/components/popup/tier/TierTag.tsx
@@ -7,33 +7,53 @@ import { WanderIcon } from "./WanderIcon";
 import { useMemo } from "react";
 import { TierTypes } from "~utils/tier/constants";
 
+const boxShadows = {
+  [TierTypes.Elite]:
+    "inset 0px 1px 1px rgba(234, 208, 131, 0.6), inset 0px 1px 2px rgba(255, 255, 255, 0.6), inset 0px 2px 12px rgba(216, 187, 81, 0.3), inset 0px 1px 8px rgba(125, 102, 35, 0.2)",
+  [TierTypes.Prime]:
+    "inset 0px 1px 1px rgba(212, 212, 212, 0.6), inset 0px 1px 2px rgba(255, 255, 255, 0.6), inset 0px 2px 12px rgba(6, 45, 60, 0.3), inset 0px 1px 8px rgba(90, 93, 94, 0.2)",
+  [TierTypes.Plus]:
+    "inset 0px 1px 1px rgba(134, 229, 169, 0.6), inset 0px 1px 2px rgba(255, 255, 255, 0.6), inset 0px 2px 12px rgba(8, 59, 88, 0.3), inset 0px 1px 8px rgba(90, 93, 94, 0.2)",
+  [TierTypes.Select]:
+    "inset 0px 1px 1px rgba(131, 215, 245, 0.6), inset 0px 1px 2px rgba(255, 255, 255, 0.6), inset 0px 2px 12px rgba(8, 59, 88, 0.3), inset 0px 1px 8px rgba(13, 136, 207, 0.2)",
+  [TierTypes.Core]:
+    "inset 0px 1px 1px rgba(151, 135, 255, 0.6), inset 0px 1px 2px rgba(255, 255, 255, 0.6), inset 0px 2px 12px rgba(107, 87, 249, 0.3), inset 0px 1px 8px rgba(107, 87, 249, 0.2)",
+};
+
 export function TierTag() {
   const { data: activeTier, isLoading } = useActiveTier();
   const { navigate } = useLocation();
 
   const tier = useMemo(() => {
-    if (isLoading || !activeTier || activeTier?.tier === TierTypes.Core) return browser.i18n.getMessage("benefits");
+    if (isLoading || !activeTier) return browser.i18n.getMessage("benefits");
     return activeTier?.tier;
   }, [activeTier, isLoading]);
 
   return (
-    <Tag onClick={() => navigate("/tier")}>
-      <WanderIcon tier={"Prime"} />
-      {isLoading ? <Loading style={{ width: "20px", height: "20px" }} /> : <Text noMargin>{tier}</Text>}
+    <Tag boxShadow={boxShadows[activeTier?.tier]} onClick={() => navigate("/tier")}>
+      <WanderIcon height={9.37} width={20} tier={activeTier?.tier} />
+      {isLoading ? (
+        <Loading style={{ width: "20px", height: "20px" }} />
+      ) : (
+        <Text weight="medium" noMargin>
+          {tier}
+        </Text>
+      )}
     </Tag>
   );
 }
 
-const Tag = styled.div`
+const Tag = styled.div<{ boxShadow: string }>`
   display: flex;
   padding: 4px 8px;
   align-items: center;
   gap: 8px;
   cursor: pointer;
   border-radius: 8px;
-  border: 1px solid rgba(255, 255, 255, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.05);
   background: ${(props) => props.theme.surfaceDefault};
-  box-shadow: 0px 0px 3.4px 0px ${(props) => props.theme.primary};
+  box-shadow: ${(props) => props.boxShadow};
+  backdrop-filter: blur(7.550000190734863px);
 
   &:hover {
     background: ${(props) => props.theme.surfaceSecondary};
--- src/components/popup/tier/WanderIcon.tsx
+++ src/components/popup/tier/WanderIcon.tsx
@@ -8,68 +8,39 @@ interface WanderIconProps {
 }
 
 const colors = {
-  [TierTypes.Elite]: { stopColorOne: "#D5AA0F", stopColorTwo: "#F7D68D" },
-  [TierTypes.Prime]: { stopColorOne: "#D5AA0F", stopColorTwo: "#F7D68D" },
-  [TierTypes.Plus]: { stopColorOne: "#90A1A5", stopColorTwo: "#E6E6E6" },
-  [TierTypes.Select]: { stopColorOne: "#C26C10", stopColorTwo: "#EDA355" },
-  [TierTypes.Core]: { stopColorOne: "#838383", stopColorTwo: "#838383" },
+  [TierTypes.Elite]: { stopColor: "white", stopColorOffsetOne: "white", stopColorOffsetTwo: "#FFDB97" },
+  [TierTypes.Prime]: { stopColor: "white", stopColorOffsetOne: "#CCCCCC", stopColorOffsetTwo: "#5E5E5E" },
+  [TierTypes.Plus]: { stopColor: "white", stopColorOffsetOne: "white", stopColorOffsetTwo: "#89CBBB" },
+  [TierTypes.Select]: { stopColor: "white", stopColorOffsetOne: "white", stopColorOffsetTwo: "#89B3CB" },
+  [TierTypes.Core]: { stopColor: "white", stopColorOffsetOne: "white", stopColorOffsetTwo: "#C3A1FF" },
 };
 
 export function WanderIcon({ tier, height = 10, width = 21 }: WanderIconProps) {
-  const { stopColorOne, stopColorTwo } = colors[tier] || colors[TierTypes.Core];
+  const { stopColor, stopColorOffsetOne, stopColorOffsetTwo } = colors[tier] || colors[TierTypes.Core];
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      style={{ flexShrink: 0 }}
       width={width}
       height={height}
+      style={{ flexShrink: 0 }}
       viewBox="0 0 21 10"
       fill="none">
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M14.3136 5.00474L10.7409 0.474544C10.5904 0.279227 10.4428 0.247329 10.2811 0.457111L6.70288 4.99737L10.1662 8.12758L10.5025 0.956044L10.8388 8.12758L14.3136 5.00474Z"
-        fill="url(#paint0_linear_4_720)"
-      />
-      <path
-        d="M16.8507 9.68666L20.4817 1.93816C20.5596 1.76843 20.3736 1.59883 20.2118 1.69201L14.7824 4.81263L11.2533 8.78827L16.8507 9.68666Z"
-        fill="url(#paint1_linear_4_720)"
-      />
-      <path
-        d="M4.1493 9.68666L0.518291 1.93816C0.440401 1.76843 0.62641 1.59883 0.788249 1.69201L6.21762 4.81263L9.7467 8.78827L4.1493 9.68666Z"
-        fill="url(#paint2_linear_4_720)"
+        d="M0.519003 1.93839C0.441114 1.76866 0.626696 1.59911 0.788534 1.6923L6.21822 4.81242L9.74752 8.788L4.14986 9.68644L0.519003 1.93839ZM20.2114 1.6923C20.3732 1.59911 20.5598 1.76866 20.4819 1.93839L16.851 9.68644L11.2534 8.788L14.7827 4.81242L20.2114 1.6923ZM10.2817 0.456948C10.4433 0.247362 10.5912 0.279265 10.7417 0.474526L14.3139 5.0048L10.8393 8.12785L10.5024 0.955972L10.1665 8.12785L6.70357 4.99699L10.2817 0.456948Z"
+        fill="url(#paint0_linear_586_442)"
       />
       <defs>
         <linearGradient
-          id="paint0_linear_4_720"
-          x1="10.4814"
-          y1="8.12758"
-          x2="10.4814"
-          y2="0.313354"
+          id="paint0_linear_586_442"
+          x1="19.0815"
+          y1="-1.03549"
+          x2="0.500992"
+          y2="10.7866"
           gradientUnits="userSpaceOnUse">
-          <stop stopColor={stopColorOne} />
-          <stop offset="1" stopColor={stopColorTwo} />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_4_720"
-          x1="12.6975"
-          y1="6.53399"
-          x2="17.5727"
-          y2="9.30331"
-          gradientUnits="userSpaceOnUse">
-          <stop stopColor={stopColorOne} />
-          <stop offset="1" stopColor={stopColorTwo} />
-        </linearGradient>
-        <linearGradient
-          id="paint2_linear_4_720"
-          x1="8.30248"
-          y1="6.53399"
-          x2="3.42726"
-          y2="9.30331"
-          gradientUnits="userSpaceOnUse">
-          <stop stopColor={stopColorOne} />
-          <stop offset="1" stopColor={stopColorTwo} />
+          <stop stopColor={stopColor} />
+          <stop offset="0.3" stopColor={stopColorOffsetOne} />
+          <stop offset="1" stopColor={stopColorOffsetTwo} />
         </linearGradient>
       </defs>
     </svg>
