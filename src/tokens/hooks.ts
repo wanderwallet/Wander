fb81f2e0ccd629d18341f0e21c396a09a69b4bb8
Pawan Paudel <pawanpaudel93@gmail.com>
2025-07-17 23:58:48 +0545

    refactor: uncomment the tanstack default options values

--- src/tokens/hooks.ts
+++ src/tokens/hooks.ts
@@ -25,8 +25,8 @@ import { retryWithDelay } from "~utils/promises/retry";
 
 export const defaultOptions = {
   refetchInterval: 300_000,
-  // staleTime: 300_000,
-  // gcTime: 300_000,
+  staleTime: 300_000,
+  gcTime: 300_000,
   retry: 3,
   retryDelay: (attemptIndex: number) => Math.min(1000 * 2 ** attemptIndex, 30000),
   refetchOnWindowFocus: true,
