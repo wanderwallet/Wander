0b881517a6529268c8fbc3f30c94f9f04dbc9da2
Pawan Paudel <pawanpaudel93@gmail.com>
2025-10-14 23:41:40 +0545

    refactor: revert Liquidops signer changes

--- src/routes/popup/agents/liquidops/utils/LiquidOps.ts
+++ src/routes/popup/agents/liquidops/utils/LiquidOps.ts
@@ -1,9 +1,9 @@
 import LiquidOps from "liquidops";
+import { ArweaveSigner, createData } from "@dha-team/arbundles";
 import { getActiveKeyfile, type DecryptedWallet } from "~wallets";
 import { isLocalWallet } from "~utils/assertions";
 import { freeDecryptedWallet } from "~wallets/encryption";
 import type { JWKInterface } from "arweave/web/lib/wallet";
-import { createDataItemSigner } from "~utils/aoconnect";
 
 export const LiquidOpsClient = async (useWallet = false) => {
   let decryptedWallet: DecryptedWallet;
@@ -18,6 +18,31 @@ export const LiquidOpsClient = async (useWallet = false) => {
           keyfile: {} as JWKInterface,
         };
     isLocalWallet(decryptedWallet);
+
+    const createDataItemSigner =
+      (wallet: any) =>
+      async ({
+        data,
+        tags = [],
+        target,
+        anchor,
+      }: {
+        data: any;
+        tags?: { name: string; value: string }[];
+        target?: string;
+        anchor?: string;
+      }): Promise<{ id: string; raw: ArrayBuffer }> => {
+        const signer = new ArweaveSigner(wallet);
+        const dataItem = createData(data, signer, { tags, target, anchor });
+
+        await dataItem.sign(signer);
+
+        return {
+          id: dataItem.id,
+          // @ts-expect-error
+          raw: dataItem.getRaw(),
+        };
+      };
 
     const signer = createDataItemSigner(decryptedWallet.keyfile);
     const client = new LiquidOps(signer);
