392df0ff8167364bad3a451d9f2cd6a020e70881
Pawan Paudel <pawanpaudel93@gmail.com>
2025-10-30 22:10:39 +0545

    fix: improve handling of process.version

--- src/contents/injected/setup-wallet-sdk.injected-script.ts
+++ src/contents/injected/setup-wallet-sdk.injected-script.ts
@@ -5,9 +5,9 @@ log(LOG_GROUP.SETUP, "injected/setup-wallet-sdk.injected-script.ts");
 
 // Added as a fix for arweave-storage-sdk issue in development mode where process.version is undefined:
 if (process.env.NODE_ENV === "development" && import.meta.env?.VITE_IS_EMBEDDED_APP !== "1") {
-  const windowProcess = (window as any).process;
-  if (windowProcess?.version === undefined) {
-    windowProcess.version = "";
+  const wp = (window as any).process;
+  if (wp && typeof wp === "object" && wp.version === undefined) {
+    wp.version = "";
   }
 }
 
